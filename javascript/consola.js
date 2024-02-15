a31471ad62c9d475cdb27a0dcc445aaf9f4e4fe7
= <=>
2024-02-15 21:25:27 +0100

    Se añade Introduccion NodeJS

---
+++ javascript/consola.js
@@ -0,0 +1,57 @@
+console.log("HolaMundo desde JS");
+
+
+//SALIDAS
+
+//********************* */
+//ADVERTENCIA EN CONSOLA
+//********************* */
+
+console.warn("Advertencia en la Consola");
+console.error("Esto es un Error");
+console.table("Esto es una tabla");
+
+agenda = [];
+agenda[0] = "Luis";
+agenda[1] = "Martin";
+
+console.table(agenda);
+
+//********************* */
+//OPERADORES ARITMETICOS
+//********************* */
+
+console.log(4+3);
+console.log(4-3);
+console.log(4*3);
+console.log(4/3);
+console.log(4%3);
+
+//************* */
+//COMPARADORES
+//************* */
+
+console.log(4<3);
+console.log(4>3);
+console.log(4==4);
+console.log(4<=3);
+console.log(4>=3);
+console.log(4!=3);
+
+    // ---> compara solo valor
+    console.log(4==4);
+    // ---> compara valor y tipo de dato
+    console.log(4==="4");
+
+//******************** */
+//OPERADORES BOOLEANOS
+//******************* */
+
+console.log(3 == 3 && 4 == 4);
+console.log(3 == 3 && 4 == 4 && 5 == 5);
+console.log(3 == 3 && 4 == 4 && 5 == 4);
+
+console.log( 3 == 3 || 4 == 4 || 5 == 5);
+console.log( 3 == 3 || 4 == 4 || 5 == 4);
+console.log( 3 == 2 || 4 == 3 || 5 == 4);
+
---
+++ javascript/variable-global.js
@@ -0,0 +1,51 @@
+
+//************** */
+// VARIABLES 
+//************** */
+
+//ASIGNACION
+var edad = 45;
+
+//ENCADENAMIENTO (+)
+console.log("Tu edad es de " + edad + " años.");
+
+//INCREMENTOS
+
+    //-> sumamos 1
+    console.log("SUMAMOS 1: " + (edad++));
+    //-> restamos 1
+    console.log("RESTAMOS 1: " + (edad--));
+
+
+//OPERADORES ABREVIADOS
+
+    //-> sumamos +3
+    console.log("SUMAMOS 3: " + (edad += 3));
+    //-> restamos 3
+    console.log("RESTAMOS 3: " + (edad -= 3));
+
+    //-> multiplicamos x2
+    console.log("MULTIPLICAMOS POR 2:  " + (edad *= 2));
+
+    //-> dividimos entre 2
+    console.log("DIVIDIMOS ENTRE 2: " + (edad /= 2));
+
+
+//VARIABLES GLOBALES
+
+    var edad = 45;
+    edad = 46;
+
+    console.log("Tu edad es de " + edad + " años.");
+
+//VARIABLES LOCALES
+
+
+    let edad1 = 45;
+    edad1 = 46;
+    console.log("Tu edad es de " + edad1 + " años.");
+
+//CONSTANTES (mayusculas)
+
+    const CONSTANTEEDAD = 45;
+    console.log("Tu edad es de " + CONSTANTEEDAD + " años.");
